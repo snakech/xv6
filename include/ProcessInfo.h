635729b26f69bdc946354517e795bbfa42291174
Zihao Chen <zihao.chen31@gmail.com>
2017-10-15 22:51:51 -0500

    clean code of xv6 before start

---
+++ include/ProcessInfo.h
@@ -0,0 +1,9 @@
+
+struct ProcessInfo {
+	int pid; 	// process ID
+	int ppid; 	// parent PID
+	int state;	// state
+	uint sz; 	//size in bytes
+	char *name;	// name of Process
+};
+
---
+++ user/ps.c
@@ -0,0 +1,11 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "ProcessInfo.h"
+
+int
+main(int argc, char *argv[])
+{
+  getprocs();
+  exit();
+}
